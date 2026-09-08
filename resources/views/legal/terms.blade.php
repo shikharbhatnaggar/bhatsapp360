171b6e222bc19ffb48421a10a3c8029e0ec92996
shikhar bhatnaggar <bhatnagar.shikhar@gmail.com>
2026-09-08 11:28:48 +0530

    Update legal terms with company details

--- resources/views/legal/terms.blade.php
+++ resources/views/legal/terms.blade.php
@@ -5,11 +5,11 @@
 @php
     // TODO: replace these values, then delete this comment.
     $product = config('app.name');
-    $company = '[LEGAL ENTITY NAME]';
-    $address = '[REGISTERED ADDRESS, CITY, STATE, PIN, INDIA]';
-    $email = '[support@yourdomain.com]';
-    $jurisdiction = '[CITY], [STATE]';
-    $effective = '[DD Month YYYY]';
+    $company = 'Shiventech Consulting';
+    $address = 'Purushottam Vihar, Golek Ka Mandir, Gwalior, MP, 474005, INDIA';
+    $email = 'support@shiventech.co.in';
+    $jurisdiction = 'Gwalior, MP, India';
+    $effective = '14 September 2026';
 @endphp
 
 <div class="mx-auto max-w-3xl px-6 py-14">
